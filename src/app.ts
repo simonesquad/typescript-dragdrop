7a619bb48a66a44f28c7e1785e37383d0f8752a7
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-01 10:46:48 -0600

    form is rendering data and inputs are clearing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
+//adding the autobind decorator so we don't need to use the bind keyword
+function autobind(
+    target: any, 
+    methodName: string, 
+    descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
+        const adjDescriptor: PropertyDescriptor = {
+            configurable: true,
+            get() {
+                const boundFn = originalMethod.bind(this);
+                return boundFn;
+            }
+        };
+        return adjDescriptor;
+    }
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -11,9 +33,52 @@ class ProjectInput {
         const importedNode = document.importNode(this.templateElement.content, true);
         // storing the html element here
         this.element = importedNode.firstElementChild as HTMLFormElement;
-        //calling the private attach method so the code below executes
-        this.attach();
+        //this line updates the form styling
+        this.element.id = 'user-input';
+        // here populating the elements listed above
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
+        //calling the private attach method so the code below executes
+        this.configure();
+        this.attach();
+    }
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+// if all these conditions are satisfied, then continue
+        if (
+            enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || 
+            enteredPeople.trim().length === 0
+        ) {
+            alert('Invalid input, please try again!');
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+// empty all inputs after submission
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
     // when we instatiate it we should be able to render the form
     private attach() {
