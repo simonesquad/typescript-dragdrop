69068e3294008609f68ad049fa7055c0bddbb988
Simone Ballard <simone.a.ballard@gmail.com>
2021-11-09 16:39:03 -0700

    updating tsconfig

---
+++ src/project-model.js
@@ -0,0 +1,19 @@
+var App;
+(function (App) {
+    var ProjectStatus;
+    (function (ProjectStatus) {
+        ProjectStatus[ProjectStatus["Active"] = 0] = "Active";
+        ProjectStatus[ProjectStatus["Finished"] = 1] = "Finished";
+    })(ProjectStatus = App.ProjectStatus || (App.ProjectStatus = {}));
+    var Project = /** @class */ (function () {
+        function Project(id, title, description, people, status) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.people = people;
+            this.status = status;
+        }
+        return Project;
+    }());
+    App.Project = Project;
+})(App || (App = {}));
